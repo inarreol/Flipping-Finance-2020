2648bf73777750a720ed93734d03da4712feb464
inarreol <56372279+inarreol@users.noreply.github.com>
2020-02-28 23:39:29 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,107 @@
+var config = {
+    style: 'mapbox://styles/inarreol/ck75p3ncw10r71ip85pq46jfx',
+    accessToken: 'pk.eyJ1IjoiaW5hcnJlb2wiLCJhIjoiY2p1ZnB2MWh5MGd1NDQ0cGozN29kYmR3NSJ9.14s63N3V8r2xcufGCZrnqw',
+    showMarkers: false,
+    theme: 'dark',
+    alignment: 'left',
+    title: 'Opportunity Zones in Bloomington',
+    subtitle: 'Flipping Finance 2020',
+    byline: 'By Ingrid Arreola & Roger Lee',
+    footer: 'Source: Source: State of Indiana, Bloomington Data',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            title: 'Opportunity Zones',
+            image: 'btown.jpg',
+            description: 'Opportunity Zones are capable of positive social impact. While challenges exist, there are novel solutions to help bridge gaps between investors and the Bloomington community. The lack of communication between investors and community can cause difficulty with navigating economic development programs, this can be challenging. There is disconnect in the ability for investors’ to have a positive “social impact". Opportunity Zone investors have clear federal incentives, as well as state & local.',
+            location: {
+                center: [-86.57722, 39.16291],
+                zoom: 12.07,
+                pitch: 0.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                 {
+                     layer: 'opportunity',
+                     opacity: 1
+                 }
+            ],
+            onChapterExit: [
+                 {
+                     layer: 'opportunity',
+                     opacity: 0
+                 }
+            ]
+        },
+        {
+            id: 'identifier-2',
+            title: 'The TIF Social Impact',
+            image: '',
+            description: 'This is the first part of the map that shows the intersection of TIF and OZ.',
+            location: {
+                center: [-86.54725, 39.16859],
+                zoom: 14.14,
+                pitch: 60,
+                bearing: -43.2
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'identifier-3',
+            title: 'Benefits of TIF in the OZ',
+            image: '',
+            description: 'This is the second part of the map that shows intersection of TIF and OZ. ',
+            location: {
+                center: [-86.54808, 39.16449],
+                zoom: 14.26,
+                pitch: 30,
+                bearing: -30.2
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'identifier-4',
+            title: 'TIF and the OZ',
+            image: '',
+            description: 'This is the third part of the map that shows intersection of TIF and OZ.',
+            location: {
+                center: [-86.54830, 39.15683],
+                zoom: 14.11,
+                pitch: 20,
+                bearing: -20
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'identifier-5',
+            title: 'Community Benefits from TIF and OZ',
+            image: '',
+            description: 'This is the fourth part of the map that shows intersection of TIF and OZ.',
+            location: {
+                center: [-86.55073, 39.14456],
+                zoom: 14.41,
+                pitch: 15,
+                bearing: -15
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'identifier-5',
+            title: 'Certified Technology Park',
+            image: '',
+            description: 'This is the fifth part of the map that shows intersection of TIF and OZ within the Certified Technology Park.',
+            location: {
+                center: [-86.54723, 39.17110],
+                zoom: 14.69,
+                pitch: 20,
+                bearing: -25
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ] 
+};
